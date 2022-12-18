3008976fe66ec2f57e93bcb629df35a3e72666de
Raja Harsh Vardhan Singh <singhrajaharshvardhan004@gmail.com>
2022-12-19 03:31:09 +0530

    feat: added few component and css files

---
+++ frontend/src/ecommerce/assets/components/LoadingScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoadingScreen = () => {
+  return (
+    <div className='loading-screen d-flex justify-center align-items-center'>
+        <div className='loading-screen-loader'></div>
+    </div>
+  )
+}
+
+export default LoadingScreen
