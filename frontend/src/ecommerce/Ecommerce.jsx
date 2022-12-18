94b2af4972ff7096ca6a8f64e17668d89875357f
Raja Harsh Vardhan Singh <singhrajaharshvardhan004@gmail.com>
2022-12-18 22:26:05 +0530

    refactor: UI updates in profile

--- frontend/src/ecommerce/Ecommerce.jsx
+++ frontend/src/ecommerce/Ecommerce.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Suspense } from 'react'
 import { Routes, Route, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
@@ -6,12 +6,21 @@ import 'react-toastify/dist/ReactToastify.css'
 
 import Layout from './assets/layouts/Layout'
 
-import ProductShow from './assets/pages/ProductShow'
+/* import ProductShow from './assets/pages/ProductShow'
 import Cart from './assets/pages/Cart'
-import NotFoundPage from './assets/components/NotFoundPage'
 import Login from './assets/components/Login'
-import Profile from './assets/pages/Profile'
-import Address from './assets/components/profile/Address'
+import Profile from './assets/pages/Profile' */
+/* import Address from './assets/components/profile/Address' */
+
+import NotFoundPage from './assets/components/NotFoundPage'
+import LoadingScreen from './assets/components/LoadingScreen'
+
+const ProductShow = React.lazy(() => import('./assets/pages/ProductShow'))
+const Cart = React.lazy(() => import('./assets/pages/Cart'))
+/* const NotFoundPage = React.lazy(() => import('./assets/components/NotFoundPage')) */
+const Login = React.lazy(() => import('./assets/components/Login'))
+const Profile = React.lazy(() => import('./assets/pages/Profile'))
+const Address = React.lazy(() => import('./assets/components/profile/Address'))
 
 const Ecommerce = () => {
     const navigate = useNavigate()
@@ -61,8 +70,10 @@ const Ecommerce = () => {
     function HomePage() {
         return (
             <Layout isAuth={isAuth} setIsAuth={setIsAuth}>
-                <Ecommerce />
-                <ProductShow isAuth={isAuth} />
+                <Suspense fallback={<LoadingScreen />}>
+                    <Ecommerce />
+                    <ProductShow isAuth={isAuth} />
+                </Suspense>
             </Layout>
         )
     }
@@ -70,7 +81,7 @@ const Ecommerce = () => {
     function Products() {
         return (
             <Layout isAuth={isAuth} setIsAuth={setIsAuth}>
-                <ProductShow isAuth={isAuth} />
+                <Suspense fallback={<LoadingScreen />}><ProductShow isAuth={isAuth} /></Suspense>
             </Layout>
         )
     }
@@ -78,7 +89,7 @@ const Ecommerce = () => {
     function CartShow() {
         return (
             <Layout isAuth={isAuth} setIsAuth={setIsAuth}>
-                <Cart isAuth={isAuth} />
+                <Suspense fallback={<LoadingScreen />}><Cart isAuth={isAuth} /></Suspense>
             </Layout>
         )
     }
@@ -92,7 +103,7 @@ const Ecommerce = () => {
     function LoginShow() {
         return (
             <Layout isAuth={isAuth} setIsAuth={setIsAuth}>
-                <Login auth={setIsAuth} />
+                <Suspense fallback={<LoadingScreen />}><Login auth={setIsAuth} /></Suspense>
             </Layout>
         )
     }
@@ -100,7 +111,7 @@ const Ecommerce = () => {
     function ProfileShow() {
         return (
             <Layout isAuth={isAuth} setIsAuth={setIsAuth} >
-                <Profile auth={setIsAuth} />
+                <Suspense fallback={<LoadingScreen />}><Profile auth={setIsAuth} /></Suspense>
             </Layout>
         )
     }
@@ -110,15 +121,16 @@ const Ecommerce = () => {
             <Route index element={<HomePage />} />
             <Route path="products" element={<Products />} />
             <Route path="cart" element={<CartShow />} />
-            {isAuth ?
-                <>
-                    <Route path="profile" element={<ProfileShow />}>
-                        <Route path="address" element={<Address />} />
-                    </Route>
-                    <Route path='login' element={<LoginRedirect />} />
-                </>
-                :
-                <Route path='login' element={<LoginShow />} />
+            {
+                isAuth ?
+                    <>
+                        <Route path="profile" element={<ProfileShow />}>
+                            <Route path="address" element={<Address />} />
+                        </Route>
+                        <Route path='login' element={<LoginRedirect />} />
+                    </>
+                    :
+                    <Route path='login' element={<LoginShow />} />
             }
             <Route path="*" element={<NotFoundPage />} />
         </Routes>
--- frontend/src/ecommerce/assets/pages/Profile.jsx
+++ frontend/src/ecommerce/assets/pages/Profile.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState, Suspense } from 'react'
 import { Link, Outlet, useOutlet, useLocation, useNavigate } from 'react-router-dom'
+import axios from 'axios'
 import { HelmetProvider, Helmet } from 'react-helmet-async'
+import { FaUserAlt, FaPowerOff } from 'react-icons/fa'
 
 import ProfileInfo from '../components/profile/ProfileInfo'
+import LoadingScreen from '../components/LoadingScreen'
 
 import ProfileStyles from './profile.module.css'
+import './profile.css'
+
+import MaleProfilePic from '../components/profile/profile-pic-male.svg'
+import FemaleProfilePic from '../components/profile/profile-pic-female.svg'
 
 
 const Profile = ({ auth }) => {
@@ -13,6 +20,29 @@ const Profile = ({ auth }) => {
     const navigate = useNavigate()
     const endPoint = useLocation().pathname.split('/').at(-1)
 
+
+    const [userDetails, setUserDetails] = useState([])
+
+    function wait(time) {
+        return new Promise(resolve => {
+            setTimeout(resolve, time)
+        })
+    }
+
+    async function getUserDetails() {
+        await axios.get(`http://localhost:4000/user/${localStorage.getItem('akey')}`).then(response => {
+            let data = response.data
+            if (data.result === false && data.statusCode === 404) {
+                console.log('User not authorized')
+            } else {
+                setUserDetails(data.data)
+            }
+        })
+    }
+    useEffect(() => {
+        getUserDetails()
+    }, [])
+
     return (
         <div className='container-2'>
             <HelmetProvider>
@@ -20,40 +50,60 @@ const Profile = ({ auth }) => {
                     <title>Profile</title>
                 </Helmet>
             </HelmetProvider>
-            <div className={'d-flex gap-2 ' + ProfileStyles.profileContainer}>
+            <div className={'d-flex gap-1 ' + ProfileStyles.profileContainer}>
                 <aside>
-                    <div className={'px-1 py-1 ' + ProfileStyles.sideBarBox}>
-                        Hello,<br />
-                        Raja Harsh Vardhan Singh
-                    </div>
-                    <div className={'px-1 py-1 ' + ProfileStyles.sideBarBox}>
+                    <div className={`${ProfileStyles.sideBarBox} ${ProfileStyles.userBox} d-flex align-items-center gap-1`}>
+                        <div>{userDetails.gender === "male" ? <img src={MaleProfilePic} alt="Male" /> : userDetails.gender === "female" ? <img src={FemaleProfilePic} alt="Female" /> : <div className={ProfileStyles.noProfile}><FaUserAlt /></div>}</div>
                         <div>
-                            Account Settings
+                            <span className={ProfileStyles.welcome}>Hello,</span>
+                            <br />
+                            <span className={ProfileStyles.username}>{userDetails.name}</span>
+                        </div>
+                    </div>
+                    <div className={ProfileStyles.sideBarBox}>
+                        <div className='border-bottom'>
+                            <div className={'d-flex flex-wrap-wrap ' + ProfileStyles.header}>
+                                <FaUserAlt />
+                                <div className={ProfileStyles.headerHeading}>Account Settings</div>
+                            </div>
                             <ul>
-                                <li><Link to="/profile" relative="route" className={endPoint === 'profile' ? 'child-selector active-child' : 'child-selector'}>Profile Information</Link></li>
-                                <li><Link to="address" className={endPoint === 'address' ? 'child-selector active-child' : 'child-selector'}>Manage Addresses</Link></li>
+                                <Link to="/profile" relative="route" className={endPoint === 'profile' ? 'child-selector active-child' : 'child-selector'}><li>Profile Information</li></Link>
+                                <Link to="address" className={endPoint === 'address' ? 'child-selector active-child' : 'child-selector'}><li>Manage Addresses</li></Link>
                             </ul>
                         </div>
-                        <div></div>
-                        <div></div>
-                        <div>
-                            <li onClick={() => auth(oldValue => {
+                        {/* <div className='border-bottom'></div>
+                        <div className='border-bottom'></div> */}
+                        <div className='border-bottom'>
+                            <div className={`d-flex flex-wrap-wrap ${ProfileStyles.logoutButton} ${ProfileStyles.header}`} onClick={() => auth(oldValue => {
                                 if (oldValue === true) {
                                     localStorage.clear()
                                     navigate('/')
                                 }
                                 return !oldValue
-                            })}>Logout</li></div>
+                            })}>
+                                <FaPowerOff />
+                                <div className={ProfileStyles.headerHeading}>
+                                    Logout
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className={'px-1 py-1 ' + ProfileStyles.sideBarBox}>
-                        Frequently Visited:
-                        <li>Change Password</li>
-                        <li>Track Order</li>
-                        <li>Help Center</li>
+                    <div className={ProfileStyles.sideBarBox}>
+                        <h6 className={ProfileStyles.smallHeading}>
+                            Frequently Visited:
+                        </h6>
+                        <ul className={'d-flex flex-wrap-wrap ' + ProfileStyles.smallFontList}>
+                            <li>Change Password</li>
+                            <li>Track Order</li>
+                            <li>Help Center</li>
+                        </ul>
                     </div>
                 </aside>
                 <div className={ProfileStyles.profileContent}>
-                    {outlet ? <Outlet /> : <ProfileInfo />}
+                    {outlet ?
+                        <Suspense fallback={<LoadingScreen />}>
+                            <Outlet />
+                        </Suspense> : <ProfileInfo />}
                 </div>
             </div>
         </div>
