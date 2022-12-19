117ff04c964a943fb2be4035de6d1fbe8846d98f
Raja Harsh Vardhan Singh <singhrajaharshvardhan004@gmail.com>
2022-12-20 00:51:32 +0530

    feat: added address css

--- frontend/src/ecommerce/assets/components/profile/Address.jsx
+++ frontend/src/ecommerce/assets/components/profile/Address.jsx
@@ -22,62 +22,77 @@ const Address = () => {
 
   function handleForm(e) {
     e.preventDefault()
+    let data = {
+      name: name.current.value,
+      mobNum: mobNum.current.value,
+      pinCode: pinCode.current.value,
+      locality: locality.current.value,
+      address: address.current.value,
+      city: city.current.value,
+      state: state.current.value,
+      landmark: landmark.current.value,
+      alternatePhoneNumber: alternatePhoneNumber.current.value
+    }
+    console.log(data)
   }
 
   function AddAddress() {
     return (
       <form onSubmit={handleForm} className={ProfileStyle.addAddressInputs}>
         <span className={ProfileStyle.addressFormHeading}>Add A New Address</span>
-        <div>
-          <div className='d-flex'>
-            <div>
-              <label htmlFor=""></label>
+        <div style={{display:'inline-block'}}>
+          <div className='d-flex gap-75'>
+            <div className={ProfileStyle.addressInputContainer}>
+              <label htmlFor="">Name</label>
               <input type="text" ref={name} required />
             </div>
-            <div>
-              <label htmlFor=""></label>
+            <div className={ProfileStyle.addressInputContainer}>
+              <label htmlFor="">10-digit mobile number</label>
               <input type="text" ref={mobNum} required />
             </div>
           </div>
-          <div className='d-flex'>
-            <div>
-              <label htmlFor=""></label>
+          <div className='d-flex gap-75'>
+            <div className={ProfileStyle.addressInputContainer}>
+              <label htmlFor="">Pincode</label>
               <input type="text" ref={pinCode} required />
             </div>
-            <div>
-              <label htmlFor=""></label>
+            <div className={ProfileStyle.addressInputContainer}>
+              <label htmlFor="">Locality</label>
               <input type="text" ref={locality} required />
             </div>
           </div>
-          <div className='d-flex'>
-            <div>
-              <textarea name="" id="" ref={address} required></textarea>
+          <div className='d-flex gap-75'>
+            <div className={ProfileStyle.addressInputContainer}>
               <label htmlFor="">Address(Area and Street)</label>
+              <textarea name="" id="" ref={address} required></textarea>
             </div>
           </div>
-          <div className='d-flex'>
-            <div>
-              <label htmlFor=""></label>
+          <div className='d-flex gap-75'>
+            <div className={ProfileStyle.addressInputContainer}>
+              <label htmlFor="">City/District/Town</label>
               <input type="text" ref={city} required />
             </div>
-            <select ref={state} required>
-              <option disabled checked>--Select State--</option>
-              <option value="UP">UP</option>
-            </select>
-          </div>
-          <div className='d-flex'>
-            <div>
-              <label htmlFor=""></label>
-              <input type="text" ref={landmark} required />
+            <div className={ProfileStyle.addressInputContainer}>
+              <label htmlFor="">State</label>
+              <select ref={state} defaultValue={'DEFAULT'} required>
+                <option value="DEFAULT" disabled>--Select State--</option>
+                <option value="UP">UP</option>
+              </select>
             </div>
-            <div>
-              <label htmlFor=""></label>
-              <input type="text" ref={alternatePhoneNumber} required />
+          </div>
+          <div className='d-flex gap-75'>
+            <div className={ProfileStyle.addressInputContainer}>
+              <label htmlFor="">Landmark (Optional)</label>
+              <input type="text" ref={landmark} />
+            </div>
+            <div className={ProfileStyle.addressInputContainer}>
+              <label htmlFor="">Alternate Phone (Optional)</label>
+              <input type="text" ref={alternatePhoneNumber} />
             </div>
           </div>
           <div>
             <p>Address Type</p>
-            <div className='d-flex' onChange={addressTypeChange}>
+            <div className='d-flex gap-75' onChange={addressTypeChange}>
               <label htmlFor="address_type_home">
                 <input type="radio" name='address-type' id='address_type_home' value="Home" required />
                 Home
