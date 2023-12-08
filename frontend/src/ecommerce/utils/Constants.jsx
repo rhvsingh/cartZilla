e48878488ce93cb7e101e0a31a306c17a777199a
Raja Harsh Vardhan Singh <singhrajaharshvardhan004@gmail.com>
2023-12-08 22:29:09 +0530

    refactor: changed backend prod api_url

--- frontend/src/ecommerce/utils/Constants.jsx
+++ frontend/src/ecommerce/utils/Constants.jsx
@@ -1,12 +1,12 @@
 const prod = {
-  url: {
-    API_URL: "https://CartZillaBackend.rhvsingh.repl.co/",
-    API_URL_USERS: "https://CartZillaBackend.rhvsingh.repl.co/",
-  },
+    url: {
+        API_URL: "https://giddy-visor-pig.cyclic.app/",
+        API_URL_USERS: "https://giddy-visor-pig.cyclic.app/",
+    },
 }
 const dev = {
-  url: {
-    API_URL: "http://localhost:4000/",
-  },
+    url: {
+        API_URL: "http://localhost:4000/",
+    },
 }
 export const config = process.env.NODE_ENV === "development" ? dev : prod
